53bcc05b4274754f8653588c60dd61c280e785e4
EricVogt93 <eric.vogt1993@gmail.com>
2023-01-24 23:14:24 +0100

    Add files via upload

---
+++ TwitchEmotes_SlightlyUnstable/Emotes.lua
@@ -0,0 +1,25 @@
+TwitchEmotes_SlightlyUnstable_Emoticons = {
+    ["babe_1"] = "babe_1",
+    ["babe_2"] = "babe_2",
+    ["babe_3"] = "babe_3",
+    ["babe_4"] = "babe_4",
+    ["heinrich"] = "heinrich",
+    ["jens"] = "jens",
+    ["eric"] = "eric",
+    ["187"] = "187",
+    ["einhorn"] = "einhorn",
+	["lucas"] = "lucas"
+}
+
+TwitchEmotes_SlightlyUnstable_Emoticons_Pack = {
+    ["babe_1"] = "Interface\\AddOns\\TwitchEmotes_SlightlyUnstable\\emotes\\babe_1.tga:28:28",
+    ["babe_2"] = "Interface\\AddOns\\TwitchEmotes_SlightlyUnstable\\emotes\\babe_2.tga:28:28",
+    ["babe_3"] = "Interface\\AddOns\\TwitchEmotes_SlightlyUnstable\\emotes\\babe_3.tga:28:28",
+    ["babe_4"] = "Interface\\AddOns\\TwitchEmotes_SlightlyUnstable\\emotes\\babe_4.tga:28:28",
+    ["heinrich"] = "Interface\\AddOns\\TwitchEmotes_SlightlyUnstable\\emotes\\heinrich.tga:28:28",
+    ["jens"] = "Interface\\AddOns\\TwitchEmotes_SlightlyUnstable\\emotes\\jens.tga:28:28",
+    ["eric"] = "Interface\\AddOns\\TwitchEmotes_SlightlyUnstable\\emotes\\eric.tga:28:28",
+    ["187"] = "Interface\\AddOns\\TwitchEmotes_SlightlyUnstable\\emotes\\187.tga:28:28",
+    ["187"] = "Interface\\AddOns\\TwitchEmotes_SlightlyUnstable\\emotes\\einhorn.tga:28:28",
+    ["187"] = "Interface\\AddOns\\TwitchEmotes_SlightlyUnstable\\emotes\\lucas.tga:28:28"
+}
