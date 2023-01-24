487c3a7228b8181f435cb87bb1c97c818b3b8717
EricVogt93 <eric.vogt1993@gmail.com>
2023-01-24 23:15:27 +0100

    Add files via upload

---
+++ Core.lua
@@ -0,0 +1,11 @@
+--Credit: https://github.com/Pewtro/TwitchEmotes_Solaris
+
+---@diagnostic disable: deprecated
+TwitchEmotes_SlightlyUnstable = LibStub("AceAddon-3.0"):NewAddon("TwitchEmotes_SlightlyUnstable", "AceConsole-3.0", "AceEvent-3.0")
+
+--Init
+function TwitchEmotes_SlightlyUnstable:OnInitialize()
+
+    TwitchEmotes_SlightlyUnstable:SetAutoComplete(true)
+
+end
---
+++ Init_Emotes.lua
@@ -0,0 +1,5 @@
+local function addEmotePacks()
+    Emoticons_RegisterPack("TwitchEmotes_SlightlyUnstable", TwitchEmotes_SlightlyUnstable_Emoticons, TwitchEmotes_SlightlyUnstable_Emoticons_Pack)
+end
+
+addEmotePacks()
